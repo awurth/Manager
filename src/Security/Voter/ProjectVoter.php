6471f754aa78a031ddc586a3c376b6f6164b928b
Alexis Wurth <alexis.wurth@pilipili-web.com>
2020-04-16 20:41:52 +0200

    :sparkles: Add Project Voter

---
+++ src/Security/Voter/ProjectVoter.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Security\Voter;
+
+use App\Entity\Project;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class ProjectVoter extends Voter
+{
+    private $authorizationChecker;
+
+    public function __construct(AuthorizationCheckerInterface $authorizationChecker)
+    {
+        $this->authorizationChecker = $authorizationChecker;
+    }
+
+    protected function supports($attribute, $subject): bool
+    {
+        return in_array($attribute, ['EDIT', 'DELETE', 'VIEW'])
+            && $subject instanceof Project;
+    }
+
+    /**
+     * @param string         $attribute
+     * @param Project        $subject
+     * @param TokenInterface $token
+     *
+     * @return bool
+     */
+    protected function voteOnAttribute($attribute, $subject, TokenInterface $token): bool
+    {
+        $user = $token->getUser();
+
+        if (!$user instanceof UserInterface) {
+            return false;
+        }
+
+        if ($this->authorizationChecker->isGranted('ROLE_ADMIN')) {
+            return true;
+        }
+
+        switch ($attribute) {
+            case 'EDIT':
+            case 'DELETE':
+                return $user === $subject->getOwner();
+            case 'VIEW':
+                return true;
+        }
+
+        return false;
+    }
+}
