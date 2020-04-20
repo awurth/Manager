6367bfa1fe2c58b6e4153dacf8c0106b6c819b4a
Alexis Wurth <alexis.wurth@pilipili-web.com>
2020-04-20 22:44:22 +0200

    :sparkles: Use select2 for select fields

--- assets/ts/main.ts
+++ assets/ts/main.ts
@@ -1,8 +1,13 @@
 import '@fortawesome/fontawesome-free/css/all.css'
 import '../scss/main.scss';
 
+import 'select2/dist/css/select2.css';
+
+import $ from 'jquery';
+import 'select2';
 import Dropdown from './dropdown';
 
 document.addEventListener('DOMContentLoaded', () => {
   new Dropdown('.dropdown');
+  $('select').select2();
 });
