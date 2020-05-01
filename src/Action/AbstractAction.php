be68d34eee0dd23cc9d5a07cc8de2d8e0ed6a3a3
Alexis Wurth <alexis.wurth@pilipili-web.com>
2020-05-01 13:31:29 +0200

    :recycle: Add ResponseTrait

--- src/Action/AbstractAction.php
+++ src/Action/AbstractAction.php
@@ -2,9 +2,6 @@
 
 namespace App\Action;
 
-use Symfony\Component\HttpFoundation\BinaryFileResponse;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
@@ -13,18 +10,5 @@ abstract class AbstractAction
     protected function createNotFoundException(string $message = 'Not Found', ?Throwable $previous = null): NotFoundHttpException
     {
         return new NotFoundHttpException($message, $previous);
-    }
-
-    protected function file($file, ?string $fileName = null, string $disposition = ResponseHeaderBag::DISPOSITION_ATTACHMENT): BinaryFileResponse
-    {
-        $response = new BinaryFileResponse($file);
-        $response->setContentDisposition($disposition, $fileName ?? $response->getFile()->getFilename());
-
-        return $response;
-    }
-
-    protected function json($data, int $status = 200, array $headers = []): JsonResponse
-    {
-        return new JsonResponse($data, $status, $headers);
     }
 }
---
+++ src/Action/ResponseTrait.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Action;
+
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+
+trait ResponseTrait
+{
+    protected function file($file, ?string $fileName = null, string $disposition = ResponseHeaderBag::DISPOSITION_ATTACHMENT): BinaryFileResponse
+    {
+        $response = new BinaryFileResponse($file);
+        $response->setContentDisposition($disposition, $fileName ?? $response->getFile()->getFilename());
+
+        return $response;
+    }
+
+    protected function json($data, int $status = 200, array $headers = []): JsonResponse
+    {
+        return new JsonResponse($data, $status, $headers);
+    }
+}
