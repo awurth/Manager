b0d32afb7c0c3b3dd92612880fabfa6812a9ad8a
Alexis Wurth <alexis.wurth@pilipili-web.com>
2020-05-05 19:19:22 +0200

    :sparkles: Project groups list pagination

--- src/Action/ListProjectGroupsAction.php
+++ src/Action/ListProjectGroupsAction.php
@@ -2,6 +2,9 @@
 
 namespace App\Action;
 
+use App\Repository\ProjectGroupRepository;
+use Doctrine\ORM\QueryBuilder;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,15 +13,35 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class ListProjectGroupsAction
 {
+    use PaginationTrait;
     use SecurityTrait;
     use TwigTrait;
 
-    public function __invoke(): Response
+    private $projectGroupRepository;
+
+    public function __construct(ProjectGroupRepository $projectGroupRepository)
+    {
+        $this->projectGroupRepository = $projectGroupRepository;
+    }
+
+    public function __invoke(Request $request): Response
     {
         $this->denyAccessUnlessLoggedIn();
 
+        $pager = $this->paginate($this->getQueryBuilder(), $request);
+
         return $this->renderPage('list-project-groups', 'app/project_group/list.html.twig', [
-            'groups' => $this->getUser()->getProjectGroups()
+            'groups' => $pager->getCurrentPageResults(),
+            'pager' => $pager
         ]);
+    }
+
+    private function getQueryBuilder(): QueryBuilder
+    {
+        return $this->projectGroupRepository->createQueryBuilder('g')
+            ->join('g.members', 'm')
+            ->where('m.user = :user')
+            ->setParameter('user', $this->getUser())
+            ->orderBy('g.createdAt', 'DESC');
     }
 }
