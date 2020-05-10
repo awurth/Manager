239d2bdb906ee75b0c563dc13a8a173bf20d6f3d
Alexis Wurth <alexis.wurth@pilipili-web.com>
2020-05-10 19:47:02 +0200

    :sparkles: Add form extension to set form label format on all forms + translations

---
+++ src/Form/Extension/LabelFormatExtension.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Form\Extension;
+
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LabelFormatExtension extends AbstractTypeExtension
+{
+    public static function getExtendedTypes(): iterable
+    {
+        return [FormType::class];
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'label_format' => 'form.label.%id%'
+        ]);
+    }
+}
