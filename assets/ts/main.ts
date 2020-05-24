3508057eb3dea210ca140f9a7efba02de6637afa
Alexis Wurth <alexis.wurth@pilipili-web.com>
2020-05-24 17:58:08 +0200

    :sparkles: Search

--- assets/ts/main.ts
+++ assets/ts/main.ts
@@ -18,4 +18,16 @@ document.addEventListener('DOMContentLoaded', () => {
     checkboxClass: 'icheckbox_flat',
     radioClass: 'iradio_flat'
   });
+
+  // Do not submit the search form unless the search query is at least 3 characters long
+  document.querySelector('#search_bar').addEventListener('submit', (event: Event) => {
+    event.preventDefault();
+
+    const form = event.currentTarget as HTMLFormElement;
+    const input = form.querySelector('input[type="search"]') as HTMLInputElement;
+
+    if (input.value.length > 2) {
+      form.submit();
+    }
+  });
 });
---
+++ src/Action/SearchAction.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Action;
+
+use App\Repository\ProjectGroupRepository;
+use App\Repository\ProjectRepository;
+use App\Repository\ServerRepository;
+use App\Upload\StorageInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/search", name="app_search")
+ */
+class SearchAction
+{
+    use SecurityTrait;
+    use TwigTrait;
+
+    private $projectGroupRepository;
+    private $projectLogoStorage;
+    private $projectRepository;
+    private $serverRepository;
+
+    public function __construct(
+        ProjectGroupRepository $projectGroupRepository,
+        ProjectRepository $projectRepository,
+        ServerRepository $serverRepository,
+        StorageInterface $projectLogoStorage
+    )
+    {
+        $this->projectGroupRepository = $projectGroupRepository;
+        $this->projectRepository = $projectRepository;
+        $this->serverRepository = $serverRepository;
+        $this->projectLogoStorage = $projectLogoStorage;
+    }
+
+    public function __invoke(Request $request): Response
+    {
+        $this->denyAccessUnlessLoggedIn();
+
+        $search = $request->query->get('search');
+
+        $groups = $projects = $servers = [];
+        if (strlen($search) > 2) {
+            $groups = $this->findProjectGroups($search);
+            $projects = $this->findProjects($search);
+            $servers = $this->findServers($search);
+        }
+
+        return $this->renderPage('search', 'app/search.html.twig', [
+            'projectLogoStorage' => $this->projectLogoStorage,
+            'groups' => $groups,
+            'projects' => $projects,
+            'servers' => $servers
+        ]);
+    }
+
+    private function findProjectGroups(string $search): array
+    {
+        return $this->projectGroupRepository->createQueryBuilder('g')
+            ->where('g.name LIKE :search')
+            ->orWhere('g.description LIKE :search')
+            ->setParameter('search', '%'.$search.'%')
+            ->getQuery()->getResult();
+    }
+
+    private function findProjects(string $search): array
+    {
+        return $this->projectRepository->createQueryBuilder('p')
+            ->where('p.name LIKE :search')
+            ->orWhere('p.description LIKE :search')
+            ->setParameter('search', '%'.$search.'%')
+            ->getQuery()->getResult();
+    }
+
+    private function findServers(string $search): array
+    {
+        return $this->serverRepository->createQueryBuilder('s')
+            ->where('s.name LIKE :search')
+            ->setParameter('search', '%'.$search.'%')
+            ->getQuery()->getResult();
+    }
+}
