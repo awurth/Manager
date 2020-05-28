eb2f18eaab1ccc0f55358e11c4c860ba75709f95
Alexis Wurth <alexis.wurth@pilipili-web.com>
2020-05-28 22:55:07 +0200

    :bento: Configure primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
+const { colors } = require('tailwindcss/defaultTheme');
+
 module.exports = {
   theme: {
     extend: {},
+    colors: {
+      ...colors,
+      primary: colors.indigo
+    }
   },
   variants: {
     borderWidth: ['responsive', 'last', 'hover', 'focus'],
